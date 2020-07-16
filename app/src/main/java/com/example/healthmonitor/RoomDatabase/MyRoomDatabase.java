42c9ccdab32b4ae57e860954097543f16d274eed
pietromiotti <pietro.miotti@gmail.com>
2020-07-16 17:56:18 +0200

    Updated Version of the database

--- app/src/main/java/com/example/healthmonitor/RoomDatabase/MyRoomDatabase.java
+++ app/src/main/java/com/example/healthmonitor/RoomDatabase/MyRoomDatabase.java
@@ -7,7 +7,7 @@ import androidx.room.TypeConverters;
 
 import com.example.healthmonitor.utils.Converters;
 
-@Database(entities = {Record.class}, version =2, exportSchema = false)
+@Database(entities = {Record.class}, version =1, exportSchema = false)
 @TypeConverters({Converters.class})
 public abstract class MyRoomDatabase extends RoomDatabase {
     public abstract MyDao myDao();
